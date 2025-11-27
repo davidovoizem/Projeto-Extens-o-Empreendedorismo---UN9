728f4ffd8bc6d41fbae9aaea7bd0d1006fbd6def
Davi <davi.dovoizem.monteiro@gmail.com>
2025-11-27 20:42:43 -0300

    Atualizar modal de localização e CSS

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const modal = document.querySelector(".modal")
-const mascara = document.querySelector (".mascara-modal")
+let mapaInicializado = false;
 
-function abrirModal (){
-    modal.style.left = '50%'
-    mascara.style.visibility = 'visible'
+function abrirModal() {
+    const caixa = document.querySelector('.caixa-modal'); 
+    const mascara = document.querySelector('.caixa-modal .mascara-modal'); 
+    caixa.style.display = 'flex';
+    mascara.style.visibility = 'visible';
+
+    if (!mapaInicializado) {
+        initMap();
+        mapaInicializado = true;
+    }
 }
 
-function esconderModal (){
-    modal.style.left = '-32%'
-    mascara.style.visibility = 'hidden'
+function esconderModal() {
+    const caixa = document.querySelector('.caixa-modal'); 
+    const mascara = document.querySelector('.caixa-modal .mascara-modal'); 
+    caixa.style.display = 'none'; 
+    mascara.style.visibility = 'hidden'; 
+}
+
+function initMap() { 
+    const uninove = { lat: -23.529497805796957, lng: -46.670847717745275 };
+    const map = new google.maps.Map(document.getElementById("map"), { 
+        zoom: 15, 
+        center: uninove,
+    });
+    const marker = new google.maps.Marker({ 
+        position: uninove, 
+        map: map, 
+        title: "UNINOVE - Campus Memorial", 
+    }); 
 }
